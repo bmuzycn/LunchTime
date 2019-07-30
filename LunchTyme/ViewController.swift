b0643378374f18323f11f46cee3024bf699ed7a8
timyuzhang <bmuzycn@gmail.com>
2019-07-30 11:08:18 -0500

    Initial Commit

---
+++ LunchTyme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LunchTyme
+//
+//  Created by Yu Zhang on 7/30/19.
+//  Copyright © 2019 Yu Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
